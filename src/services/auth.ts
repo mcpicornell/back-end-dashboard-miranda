0af0ef66641a992287d91e8cb1b6c333efb20492
mcpicornell <marc.castro.picornell@gmail.com>
2023-05-26 10:32:13 +0200

    controllers

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -12,38 +12,38 @@ const ExtractJWT = require('passport-jwt').ExtractJwt;
 
 
 
-// passport.use(
-//   'login',
-//   new localStrategy(
-// 	{
-//   	usernameField: 'email',
-//   	passwordField: 'password'
-// 	},
-// 	async (email, password, done) => {
-//   	try {
-//     	if (email === 'admin@admin.com' && password === '1234')
-//    		 return done(null, {email: ‘admin@admin.com’}, { message: 'Logged in Successfully' });	 
-//   	return done(null, false, { message: 'Invalid credentials' });
-//   	} catch (error) {
-//     	return done(error);
-//   	}
-// 	}
-//   )
-// );
+passport.use(
+  'local',
+  new localStrategy(
+	{
+  	usernameField: 'email',
+  	passwordField: 'password'
+	},
+	async (email: any, password: any, done: any) => {
+  	try {
+    	if (email === 'admin@admin.com' && password === '1234')
+   		 return done(null, {email: "admin@admin.com"}, { message: 'Logged in Successfully' });	 
+  	return done(null, false, { message: 'Invalid credentials' });
+  	} catch (error) {
+    	return done(error);
+  	}
+	}
+  )
+);
 
 
-// passport.use(
-//   new JWTstrategy(
-// 	{
-//   	secretOrKey: ‘CAMBIAME_ASAP’,
-//   	jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken('secret_token')
-// 	},
-// 	async (token, done) => {
-//   	try {
-//     	return done(null, token.user);
-//   	} catch (error) {
-//     	done(error);
-//   	}
-// 	}
-//   )
-// );
+passport.use(
+  new JWTstrategy(
+	{
+  	secretOrKey: "CAMBIAME_ASAP",
+  	jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken('secret_token')
+	},
+	async (token: any, done: any) => {
+  	try {
+    	return done(null, token.user);
+  	} catch (error) {
+    	done(error);
+  	}
+	}
+  )
+);
